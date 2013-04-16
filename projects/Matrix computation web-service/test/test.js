181a3859eaee55237f2466d7bfe67dcbb390715c
H4ml3t <meniluca@gmail.com>
2013-04-16 21:56:40 +0200

    test folder + import fix

---
+++ projects/Matrix computation web-service/test/test.js
@@ -0,0 +1,12 @@
+var date = new Date();
+
+console.log();
+console.log(date.getHours()+":"+"This is a test");
+console.log("You are executing test.js");
+console.log();
+
+console.log("Generate matrix with ones.");
+var matrixWithOnes = lar.utils.ones(6,5);
+
+console.log(matrixWithOnes);
+

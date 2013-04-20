515394970338b48e51b66e22d6ca4d819b1983bc
meniluca <meniluca@gmail.com>
2013-04-20 13:55:17 +0200

    url fix for external test

--- projects/Matrix computation web-service/test/test.js
+++ projects/Matrix computation web-service/test/test.js
@@ -45,4 +45,5 @@ log("test: matrix_util");
 var Ajson = { "ROWCOUNT" : 3, "COLCOUNT" : 3, "ROW" : [0,1,3,5], "COL" : [2,0,2,0,1], "DATA" : [1,1,1,1,1] };
 var Bjson = { "ROWCOUNT" : 3, "COLCOUNT" : 3, "ROW" : [0,1,3,5], "COL" : [2,0,2,0,1], "DATA" : [1,1,1,1,1] };
 
+matrix_util.url = "http://webpdb.dia.uniroma3.it/service/test/multiply";
 matrix_util.prodMatrixAsync_log(Ajson,Bjson);

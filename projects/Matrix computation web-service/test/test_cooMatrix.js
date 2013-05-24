6497cb987609ca1e339725515817a0fb3aaf9ceb
H4ml3t <meniluca@gmail.com>
2013-05-24 16:37:14 +0200

    initialized

--- projects/Matrix computation web-service/test/test_cooMatrix.js
+++ projects/Matrix computation web-service/test/test_cooMatrix.js
@@ -1,7 +1,6 @@
 /*
 
-	This is a brief example of how to use the prodMatrix function
-	available on the lib matrix_remote_product.js
+	This is a brief example of how to use coo.js
 
 	Github link : https://github.com/cvdlab-bio/weblar/tree/master/projects/Matrix%20computation%20web-service
 
@@ -12,8 +11,3 @@ log("You are executing test_cooMatrix.js");
 log("====================================");
 
 log("");
-
-matrix_remote_product.url = "http://webpdb.dia.uniroma3.it/service/test/multiply";
-
-log("Service must be available at the url: " + matrix_remote_product.url);
-

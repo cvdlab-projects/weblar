40a803ecd9124a0d81c8659e563e1b8e15034e64
Faber89 <fabri.rebecca@gmail.com>
2013-06-05 13:41:27 +0200

    Test added

---
+++ projects/Matrix computation web-service/test/test_final_coo.js
@@ -0,0 +1,15 @@
+/*
+
+	In this file there are some text to test the performace of the
+	matrix_util_accel and matrix_util_accel_json layer
+
+	You can find this library in the 'support' folder of the project on github.
+
+	Github link : https://github.com/cvdlab-bio/weblar/tree/master/projects/Matrix%20computation%20web-service
+
+
+
+log("====================================");
+log("You are executing test_final_coo.js");
+log("====================================");
+*/

2466d21311f7b4617222db8d07b4adbd4f4074d9
H4ml3t <meniluca@gmail.com>
2013-06-11 01:18:57 +0200

    cleaned

--- projects/Matrix computation web-service/lib/matrix_product.js
+++ projects/Matrix computation web-service/lib/matrix_product.js
@@ -7,124 +7,105 @@
 */
 
 !(function (exports){
-    
-    var mrp = exports.matrix_remote_product = {};
+	
+	var mrp = exports.matrix_remote_product = {};
 
-    mrp.url = "http://cvd01.dia.uniroma3.it:3000/service/test/multiply";
+	mrp.url = "http://cvd01.dia.uniroma3.it:3000/service/test/multiply";
 
-    /**
-        Verifica se è rispettato il pattern { "ROWCOUNT" : 3, "COLCOUNT" : 3, "ROW" : [0,1,3,5], "COL" : [2,0,2,0,1], "DATA" : [1,1,1,1,1] };
-        effettuando controlli sulle chiavi
-    */
-    var isValidJsonMatrix = function(){
-        // TODO
-        return true;
-    }
-
-
-    /**
-        Verifica se è rispettato il pattern { "ROWCOUNT" : 3, "COLCOUNT" : 3, "ROW" : [0,1,3,5], "COL" : [2,0,2,0,1], "DATA" : [1,1,1,1,1] };
-        effettuando controlli sul contenuto delle chiavi
-    */
-    var areMatrixValid = function(matrixA,matrixB){
-        //TODO
-        if ( !isValidJsonMatrix(matrixA) || !isValidJsonMatrix(matrixB) )
-            return false;
-
-        return true;
-    }
+	/**
+		Verifica se è rispettato il pattern { "ROWCOUNT" : 3, "COLCOUNT" : 3, "ROW" : [0,1,3,5], "COL" : [2,0,2,0,1], "DATA" : [1,1,1,1,1] };
+		effettuando controlli sulle chiavi
+	*/
+	var isValidJsonMatrix = function(){
+		// TODO
+		return true;
+	}
 
 
-    /**
-
-    */
-    var sendRequest = function(matrixA,matrixB,callback,asynchronous){ 
-
-        $.ajax({
-            type: 'POST' ,
-            url: mrp.url ,
-            data :  "matrixa=" + JSON.stringify(matrixA) + "&matrixb=" + JSON.stringify(matrixB) ,
-            async : asynchronous ,
-            processData : false ,
-            success : callback , 
-            error : function(req, status, ex) {} ,
-            timeout:60000
-        });
-
-    }
+	/**
+		Verifica se è rispettato il pattern { "ROWCOUNT" : 3, "COLCOUNT" : 3, "ROW" : [0,1,3,5], "COL" : [2,0,2,0,1], "DATA" : [1,1,1,1,1] };
+		effettuando controlli sul contenuto delle chiavi
+	*/
+	var areMatrixValid = function(matrixA,matrixB){
+		//TODO
+		if ( !isValidJsonMatrix(matrixA) || !isValidJsonMatrix(matrixB) )
+			return false;
+		return true;
+	}
 
 
-    /**
+	/**
 
-    */
-    var sync_result = mrp.sync_result = {};
-    
-    /**
-        Synchronous method that implements a POST request to the rest webserver.
-        It stores the result in the "sync_result" variable.
-    */
-    var sendRequestSync = function(matrixA,matrixB){
+	*/
+	var sendRequest = function(matrixA,matrixB,callback,asynchronous){ 
+		$.ajax({
+			type: 'POST' ,
+			url: mrp.url ,
+			data :  "matrixa=" + JSON.stringify(matrixA) + "&matrixb=" + JSON.stringify(matrixB) ,
+			async : asynchronous ,
+			processData : false ,
+			success : callback , 
+			error : function(req, status, ex) {} ,
+			timeout:60000
+		});
+	}
 
-        if ( !areMatrixValid(matrixA,matrixB) )
-            throw new Error(); //TODO
 
-        var callback = function(data) {
-            mrp.sync_result = data;
-        };
+	/**
 
-        sendRequest(matrixA,matrixB,callback,false);
+	*/
+	var sync_result = mrp.sync_result = {};
+	
+	/**
+		Synchronous method that implements a POST request to the rest webserver.
+		It stores the result in the "sync_result" variable.
+	*/
+	var sendRequestSync = function(matrixA,matrixB){
+		if ( !areMatrixValid(matrixA,matrixB) )
+			throw new Error(); //TODO
+		var callback = function(data) {
+			mrp.sync_result = data;
+		};
+		sendRequest(matrixA,matrixB,callback,false);
+	};
 
-    };
+	/**
+		Asynchronous method that implements a POST request to the rest webserver.
+		On success will be executed the "callback" function
+	*/
+	var sendRequestAsync = function(matrixA,matrixB,callback){
+		if ( !areMatrixValid(matrixA,matrixB) )
+			throw new Error(); //TODO
+		sendRequest(matrixA,matrixB,callback,true);
+	};
 
-    /**
-        Asynchronous method that implements a POST request to the rest webserver.
-        On success will be executed the "callback" function
-    */
-    var sendRequestAsync = function(matrixA,matrixB,callback){
+	/**
 
-        if ( !areMatrixValid(matrixA,matrixB) )
-            throw new Error(); //TODO
+	*/
+	mrp.prodMatrixSync = function (matrixA,matrixB) {
+		sendRequestSync(matrixA,matrixB);
+	}
 
-        sendRequest(matrixA,matrixB,callback,true);
+	/**
 
-    };
+	*/
+	mrp.prodMatrixSync_log = function (matrixA,matrixB) {
+		sendRequestSync(matrixA,matrixB);
+		console.log(this.sync_result);
+	}
 
-    /**
+	/**
 
-    */
-    mrp.prodMatrixSync = function (matrixA,matrixB) {
-        
-        sendRequestSync(matrixA,matrixB);
+	*/
+	mrp.prodMatrixAsync = function (matrixA,matrixB,callback) {
+		sendRequestAsync(matrixA,matrixB,callback);
+	}
 
-    }
+	/**
 
-    /**
-
-    */
-    mrp.prodMatrixSync_log = function (matrixA,matrixB) {
-        
-        sendRequestSync(matrixA,matrixB);
-
-        console.log(this.sync_result);
-
-    }
-
-    /**
-
-    */
-    mrp.prodMatrixAsync = function (matrixA,matrixB,callback) {
-        
-        sendRequestAsync(matrixA,matrixB,callback);
-
-    }
-
-    /**
-
-    */
-    mrp.prodMatrixAsync_log = function (matrixA,matrixB) {
-
-        sendRequestAsync(matrixA,matrixB, function(data) { console.log(data); });
-        
-    }
+	*/
+	mrp.prodMatrixAsync_log = function (matrixA,matrixB) {
+		sendRequestAsync(matrixA,matrixB, function(data) { console.log(data); }); 
+	}
 
 }(this));
